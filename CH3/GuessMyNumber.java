b2d127aa297c4e19adc475af17011fc2bd3d6039
sychoi25 <48307649+sychoi25@users.noreply.github.com>
2019-07-12 09:47:44 +0900

    Add files via upload

---
+++ CH3/GuessMyNumber.java
@@ -0,0 +1,31 @@
+package ch3;
+
+import java.util.*;
+import static java.lang.Math.abs;
+
+
+
+public class GuessMyNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		int guess;
+		Random rnd = new Random();
+		int ans = rnd.nextInt(100)+1;
+		//whie(true){
+		System.out.println("Guess what?!");
+		System.out.print("I think ");
+		guess = scn.nextInt();
+		System.out.println("So, your answer is "+guess);
+		
+		
+		if(guess == ans) {
+			System.out.println("Correct!! Well done:)");
+		}else {
+			System.out.println("Try again:(");
+			System.out.println("Your answer is off by "+Math.abs(guess-ans));
+		}
+	}
+}
