909de79bea6ecd417560bbc71ae2917efb2191cc
sychoi25 <48307649+sychoi25@users.noreply.github.com>
2019-07-12 11:32:03 +0900

    Add files via upload

---
+++ Ch04_VoidMethods/Ex_Zool.java
@@ -0,0 +1,19 @@
+package ch4;
+
+public class Ex_Zool {
+	
+	public static void zool(int i, String s1, String s2) {
+		//*System.out.println(i,s1,s2);
+		System.out.println(i);
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+
+	/*
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//*System.out.println(zool(11,"pet name","street name"));
+		zool(11, "pet name", "street name");
+	}
+*/
+}
